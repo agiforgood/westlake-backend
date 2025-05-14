a205d4d74cddd7e077d878969ef7c195ff6e750c
Blink Chen <kingcyk@enjoymusic.ai>
2025-05-14 15:40:55 +0800

    update: integrate alicloud sdk with cjs

--- src/lib/moderation.ts
+++ src/lib/moderation.ts
@@ -1,30 +1,30 @@
-import Green20220302, * as $Green20220302 from '@alicloud/green20220302';
-import * as $OpenApi from '@alicloud/openapi-client';
-import * as $Util from '@alicloud/tea-util';
-import Credential, * as $Credential from '@alicloud/credentials';
+const Green20220302 = require('@alicloud/green20220302');
+const OpenApi = require('@alicloud/openapi-client');
+const Util = require('@alicloud/tea-util');
+const Credential = require('@alicloud/credentials');
 
-const credentialsConfig = new $Credential.Config({
+const credentialsConfig = new Credential.Config({
     type: 'access_key',
-    accessKeyId: process.env.ALIYUN_ACCESSKEY_ID || "",
-    accessKeySecret: process.env.ALIYUN_ACCESSKEY_SECRET || "",
+    accessKeyId: process.env.ALIYUN_ACCESSKEY_ID,
+    accessKeySecret: process.env.ALIYUN_ACCESSKEY_SECRET,
 });
-let credential = new Credential(credentialsConfig);
-let config = new $OpenApi.Config({
-    credential: credential,
+const credentialClient = new Credential.default(credentialsConfig);
+let config = new OpenApi.Config({
+    credential: credentialClient,
 });
-config.endpoint = `green-cip.cn-shanghai.aliyuncs.com`;
-let client = new Green20220302(config);
+config.endpoint = 'green-cip.cn-shanghai.aliyuncs.com';
+const client = new Green20220302.default(config);
 
 export async function textModeration(content: string): Promise<{ isGood: boolean, labels: string }> {
-    let textModerationRequest = new $Green20220302.TextModerationRequest({
-        service: "chat_detection",
+    let textModerationRequest = new Green20220302.TextModerationRequest({
+        service: 'chat_detection',
         serviceParameters: JSON.stringify({
             content: content
         }),
     });
-    let runtime = new $Util.RuntimeOptions({});
+    let runtime = new Util.RuntimeOptions({});
     try {
-        let response = await client.textModerationWithOptions(textModerationRequest, runtime);
+        const response = await client.textModerationWithOptions(textModerationRequest, runtime);
         const labels = response.body?.data?.labels;
         if (labels && labels.length > 0) {
             return {
